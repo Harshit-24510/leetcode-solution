7d3792d553963ddcde343efc81a548e70f6b4566
Harshit Pareta <24510@iiitu.ac.in>
2026-06-27 03:15:10 +0530

    Time: 74 ms (46.18%), Space: 114.8 MB (40.15%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-    vector<int> bit;
-    int sz;
-
-    void update(int i) {
-        for (; i < sz; i += i & -i)
-            bit[i]++;
-    }
-
-    int query(int i) {
-        int s = 0;
-        for (; i > 0; i -= i & -i)
-            s += bit[i];
-        return s;
-    }
-
     long long countMajoritySubarrays(vector<int>& nums, int target) {
-        int n = nums.size();
-        sz = 2 * n + 5;
-        int offset = n + 2;
-        bit.assign(sz, 0);
+        unordered_map<int,int>mp;
+        int cumsum=0;
+        mp[0]=1;
+        long long left=0;
+        long long ans=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]==target){
+                left+=mp[cumsum];
+                cumsum+=1;
+    
+            }
+            else{
+                cumsum-=1;
+                left-=mp[cumsum];
+            }
+            mp[cumsum]+=1;
+            ans+=left;
 
-        update(offset);  // register prefix[0] = 0
-        int prefix = 0;
-        long long ans = 0;
-
-        for (int x : nums) {
-            prefix += (x == target) ? 1 : -1;
-            ans += query(prefix + offset - 1);  // count prefix sums < current
-            update(prefix + offset);
         }
-
         return ans;
+
+        
     }
 };
