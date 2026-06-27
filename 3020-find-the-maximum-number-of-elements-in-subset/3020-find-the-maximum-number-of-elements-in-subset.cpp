4a7effe37c6523040a73525d01ab2d6443b297da
Harshit Pareta <24510@iiitu.ac.in>
2026-06-28 02:46:09 +0530

    Time: 163 ms (18.41%), Space: 126.8 MB (15.92%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -1,27 +1,52 @@
+#define ll long long int
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        const int maxNum = *max_element(nums.begin(), nums.end());
-        unordered_map<long long, int> cnt;
-        for (int x : nums) cnt[x]++;
-
-        int ans = cnt.count(1) ? cnt[1] - (cnt[1] % 2 == 0 ? 1 : 0) : 1;
-
-        for (auto& [base, freq] : cnt) {
-            if (base == 1) continue;
-
-            int length = 0;
-            long long x = base;
-
-            while (x <= maxNum && cnt.count(x) && cnt[x] >= 2) {
-                length += 2;
-                x *= x;
+        map<ll,int> freq;
+        int one=0;
+        for (auto it : nums) 
+        {
+            if(it==1)
+            {
+                one++;
             }
-
-            length += (cnt.count(x) && cnt[x] >= 1) ? 1 : -1;
-            ans = max(ans, length);
+            else{
+                freq[it]++;
+            }
         }
 
+        if(one!=0 && one%2==0)
+        {
+            one--;
+        }
+        int ans=one;
+
+        for (auto it : freq)
+        {
+            ll val = it.first;
+            ll base = sqrt(val);
+
+            if ((base*base == val) && freq.count(base) && freq[base] > 1) 
+            {
+                continue;
+            }
+
+            int count = 0;
+
+            while (freq.count(val) && freq[val] > 1) 
+            {
+                count += 2;
+                val=(val*1LL*val);
+            }
+            if(freq[val]==1)
+            {
+                count++;
+            }
+            else{
+                count--;
+            }
+            ans = max(ans, count);
+        }
         return ans;
     }
 };
