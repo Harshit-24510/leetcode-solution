34eb20e381a3354ed5c1d6b8bbc8c04d61d8179a
Harshit Pareta <24510@iiitu.ac.in>
2026-06-29 01:47:57 +0530

    Time: 19 ms (62.02%), Space: 114.6 MB (77.52%) - LeetHub

--- 3153-sum-of-digit-differences-of-all-pairs/3153-sum-of-digit-differences-of-all-pairs.cpp
+++ 3153-sum-of-digit-differences-of-all-pairs/3153-sum-of-digit-differences-of-all-pairs.cpp
@@ -3,18 +3,20 @@ using ll = long long;
 class Solution {
 public:
     long long sumDigitDifferences(vector<int>& nums) {
-        int n = nums.size();
         int len = to_string(nums[0]).size();
         ll ans = 0;
+        int place = 1;
 
         for (int pos = 0; pos < len; pos++) {
             vector<int> cnt(10, 0);
 
-            for (int i = 0; i < n; i++) {
-                int digit = (nums[i] / (int)pow(10, pos)) % 10;
+            for (int i = 0; i < nums.size(); i++) {
+                int digit = (nums[i] / place) % 10;
                 ans += i - cnt[digit];
                 cnt[digit]++;
             }
+
+            place *= 10;
         }
 
         return ans;
