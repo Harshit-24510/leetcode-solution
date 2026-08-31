46128629e4325a8ea2f02d3c061ad7ca2f186e24
Harshit Pareta <24510@iiitu.ac.in>
2026-09-01 02:44:24 +0530

    Time: 10 ms (14.36%), Space: 128.5 MB (18.19%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
         ListNode* curr = head->next;
         ListNode* prev = head;
-        vector<pair<int, int>> v;
+        vector<int> v;
         int index = 1;
         while (curr != NULL && curr->next != NULL) {  
             if (curr->val > prev->val && curr->val > curr->next->val) {
-                v.push_back({curr->val, index});
+                v.push_back(index);
             }           
             if (curr->val < prev->val && curr->val < curr->next->val) {
-                v.push_back({curr->val, index});
+                v.push_back(index);
             }
             prev = curr;
             curr = curr->next;
@@ -21,9 +21,9 @@ public:
         }
         int mini = INT_MAX;
         for (int i = 1; i < v.size(); i++) {
-            mini = min(mini, v[i].second - v[i - 1].second);
+            mini = min(mini, v[i] - v[i - 1]);
         }
-        int maxi = v.back().second - v.front().second;
+        int maxi = v.back()- v.front();
         return {mini, maxi};
     }
 };
