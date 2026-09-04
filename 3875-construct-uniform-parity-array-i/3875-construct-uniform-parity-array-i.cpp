058553e2706b60a2c76615ac0595746af7870820
Harshit Pareta <24510@iiitu.ac.in>
2026-09-04 18:27:15 +0530

    Time: 0 ms (100%), Space: 30.3 MB (63.67%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int zero=0;
-        int n=nums1.size();
-        for(int i=0;i<nums1.size();i++){
-            if(nums1[i]==0) zero++;
-        }
-        if(zero==n) return false;
-        else return true;
+        return true;
         
     }
 };
