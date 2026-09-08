da822483dd6f2063c3145e8c22128751f1440962
Harshit Pareta <24510@iiitu.ac.in>
2026-09-08 16:48:49 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int comma = 0;
+        if (n < 1000)
+            return 0;
 
-        for (int i = 1000; i <= n; i++) {
-            int x = i;  
-
-            while (x >= 1000) {
-                x = x / 1000;
-                comma++;
-            }
-        }
-
-        return comma;
+        return (n - 1000) + 1;
     }
 };
